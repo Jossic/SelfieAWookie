65f946836d5677f5cf5143ba37f35854f7880428
Jossic <jossic.lapierre@marvin-bot.com>
2022-07-29 10:13:33 +0200

    feature: Test Get Method

--- TestWebApi/SelfieControllerSpecs.cs
+++ TestWebApi/SelfieControllerSpecs.cs
@@ -1,9 +1,22 @@
+using FluentAssertions;
+using SelfieAWookie.API.UI.Controllers;
+
 namespace TestWebApi;
 
 public class SelfieControllerSpecs
 {
     [Fact]
-    public void Test1()
+    public void Should_return_a_list_of_selfies()
     {
+        // Arrange
+        var controller = new SelfieController();
+        
+        
+        // Act
+        var result = controller.Get().ToList();
+
+        // Assert
+        result.Should().NotBeNull();
+        result.GetEnumerator().MoveNext().Should().BeTrue();
     }
 }
